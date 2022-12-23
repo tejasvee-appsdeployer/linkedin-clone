6414a7e14272ab7f782e68968aac3cf08211f7c2
shrinivas Joshi appdeployer <shrinivas@appsdeployer.com>
2022-12-23 12:29:14 +0530

    conflict resolved

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -7,6 +7,7 @@ import SignUp from './components/SignUp';
 import Feed from './components/Feed';
 import Protected from './Protected';
 import BlogPost from './components/BlogPost';
+import Home from './components/Home/Home'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <ToastContainer position='top-center' />
         <Routes>
           <Route path='/' element={<Protected Component={Feed} />} />
+          <Route path='/home' element={<Home/>}/>
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/blogpost' element={<BlogPost />} />
---
+++ linkedin/src/components/Home/ProfileCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import profile from '../../Images/profile.jpg'
+import './ProfileCard.scss';
+import { SocialIcon } from 'react-social-icons';
+
+
+const ProfileCard = () => {
+  return (
+    <>
+      <div className="card-wrap" >
+        <div className="img-wrap">
+          <img src={profile} className="" alt="profile" />
+        </div>
+        <div className="card-body">
+            <h4>Abhimanyu Sharma</h4>
+            <h5 style={{fontWeight:'200'}}>Computer Science Engineer</h5>
+            <div className="social">
+                <SocialIcon style={{height:'35px',width:'35px'}}  className="social-icon" network="dribbble"/>
+                <SocialIcon style={{height:'35px',width:'35px'}}  className="social-icon" network="instagram"/>
+                <SocialIcon style={{height:'35px',width:'35px'}}  className="social-icon" network="facebook"/>
+                <SocialIcon style={{height:'35px',width:'35px'}}  className="social-icon" network="google"/>
+            </div>
+            <button className="btn btn-primary" style={{borderRadius:'100px' ,width:'100%'}}>View profile</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileCard;
