a6c257714c34e38cd413bdaa7c1b91d292f4b0e7
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-26 11:14:18 +0530

    firebase change

--- linkedin/src/firebase.js
+++ linkedin/src/firebase.js
@@ -1,27 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvB63z9XIjjr0hITusquzHVEfotJDhG0k",
-  authDomain: "connectin-4d628.firebaseapp.com",
-  projectId: "connectin-4d628",
-  storageBucket: "connectin-4d628.appspot.com",
-  messagingSenderId: "1010749080819",
-  appId: "1:1010749080819:web:7bb2fd57eb061f755f6386"
+	apiKey: "AIzaSyDaropDCLadWq_C0jT8ppfc-WZ77yBG-bU",
+	authDomain: "connectin-c7f41.firebaseapp.com",
+	projectId: "connectin-c7f41",
+	storageBucket: "connectin-c7f41.appspot.com",
+	messagingSenderId: "324264830639",
+	appId: "1:324264830639:web:997f1a832dc122d765165a",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const userAuth = getAuth();
 const database = getFirestore(app);
 const storage = getStorage(app);
 
-
-export {app, userAuth, database, storage};
+export { app, userAuth, database, storage };
