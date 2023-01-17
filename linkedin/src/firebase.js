59e1ef1ddb6ccca15b687d71cdf9ee6db247f7ed
Kartik-Appsdeployer <115976130+Kartik-appsdeployer@users.noreply.github.com>
2023-01-17 10:46:10 +0530

    Update firebase.js

--- linkedin/src/firebase.js
+++ linkedin/src/firebase.js
@@ -8,14 +8,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCPWJJSTO1ksqlYfyOYQc3krmm3OEVcwe0",
-  authDomain: "connectin-dacbf.firebaseapp.com",
-  projectId: "connectin-dacbf",
-  storageBucket: "connectin-dacbf.appspot.com",
-  messagingSenderId: "488403830313",
-  appId: "1:488403830313:web:12abd4a3cbedcb5515be38"
-};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const userAuth = getAuth();
